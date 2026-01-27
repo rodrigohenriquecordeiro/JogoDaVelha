aa7d2b7270257fdfa4d6369cbf8e40b8b80d20cf
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2026-01-27 19:21:26 -0300

    feat: Cria método de click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,23 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'JogoDaVelha';
+  tabuleiro: string[] = Array(9).fill('');
+  proximoJogador: string = 'X';
+  posicao: number | undefined;
+
+  aoClicar(indice: number) {
+    if (!this.tabuleiro[indice]) {
+      this.tabuleiro[indice] = this.proximoJogador;
+      this.proximoJogador = this.proximoJogador === 'X' ? 'O' : 'X';
+       
+      this.posicao = indice;
+      console.log('posicao: ', this.posicao, 'jogada: ', this.tabuleiro[indice])
+
+    }
+  }
 }
